5f96fbb5e28ccfd594c15998b64a5cc0451dc265
Ross <ross.lillis@gmail.com>
2025-04-29 20:31:11 +1000

    chore: remove log

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,8 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Profile from "./pages/Profile";
 import { AuthProvider } from "./hooks/use-auth";
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import outputs from "amplify_outputs.json";
 Amplify.configure(outputs);
-console.log('outputs', outputs)
 
 const queryClient = new QueryClient();
 
